a1f10921bb38e7876b18fe9fabc08ec894e463a5
Eric Kunzel <kunzel.eric@gmail.com>
2023-10-07 19:12:39 -0300

    feat: finished testing POST route

--- src/module/aluno/__tests__/aluno.int.test.ts
+++ src/module/aluno/__tests__/aluno.int.test.ts
@@ -1,13 +1,51 @@
 import request from 'supertest';
 const {getType} = require('jest-get-type');
 
+const baseURL = "http://localhost:8080";
+
 describe('Int - Aluno Suite', () => {
-  it('##GET /alunos - Deve retornar alunos cadastrados', async () => {
-    const response = await request('http://localhost:8080').get('/aluno');
+  it('##GET /alunos', async () => {
+    const response = await request(baseURL).get('/aluno');
 
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toMatch(/json/);
-
     expect(getType(response.body.data)).toBe('array');
+  })
+
+  it('##POST /alunos', async () => {
+    const aluno = {
+      nome: "João da Silva",
+      cpf: 123456789
+    }
+
+    const response = await request(baseURL).post('/aluno').send(aluno);
+
+    expect(response.status).toBe(200);
+  })
+
+  it('##POST /alunos - faltando campos de nome e CPF do aluno', async () => {
+    const response = await request(baseURL).post('/aluno').send();
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({ error: "Inserir informações nos campos."})
+  })
+
+  it('##POST /alunos - faltando campos de nome de aluno', async () => {
+    const response = await request(baseURL).post('/aluno').send({
+      nome: "",
+      cpf: 123456789
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({ error: "Inserir nome de aluno."})
+  })
+
+  it('##POST /alunos - faltando campos de CPF de aluno', async () => {
+    const response = await request(baseURL).post('/aluno').send({
+      nome: "João da Silva",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({ error: "Inserir CPF de aluno."})
   })
 })
--- src/module/aluno/aluno.route.ts
+++ src/module/aluno/aluno.route.ts
@@ -10,5 +10,19 @@ router.get('/', async (req: Request, res: Response) => {
 
 router.post('/', async (req: Request, res: Response) => {
   const data = await alunoFactory.store(req.body);
+  const { nome, cpf } = req.body;
+
+  if(!nome && !cpf) {
+    return res.status(400).json({ error: "Inserir informações nos campos."})
+  }
+  
+  if(!nome) {
+    return res.status(400).json({ error: "Inserir nome de aluno."})
+  }
+
+  if(!cpf) {
+    return res.status(400).json({ error: "Inserir CPF de aluno."})
+  }
+
   return res.status(200).json({ data });
 });
